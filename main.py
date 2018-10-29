a4664920b164af9e46fb659c1901ea6eb65a6fc0
AmirHosein <cando428@gmail.com>
2018-10-29 23:26:37 +0330

    Add argument parser

--- main.py
+++ main.py
@@ -2,9 +2,17 @@ from sys import argv
 from sys import exit as sysexit
 from time import sleep
 from attacker import Attacker
+import argparse
 
-username = argv[1]
-passlist = argv[2]
+strings = {
+    'DESCRIPTION' : 'A simple tool for doing brute-force attack on instagram accounts, with support for tor network.'
+}
+
+parser = argparse.ArgumentParser(description = strings['DESCRIPTION'])
+
+
+# username = argv[1]
+# passlist = argv[2]
 # thread_c = int(argv[3])
 
 attacker = Attacker(username, passlist)
